3e57d7732bec7d8fe61b047544d4312f031f6e0b
DESKTOP-JI1JKUA\ibrahim <donmez2k@gmail.com>
2024-01-30 20:46:20 +0300

    Add project files.

---
+++ IkinciClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorInjectionProject
+{
+    public class IkinciClass
+    {
+        IMesajlaClass _oIMesajlaClass = null;
+
+        public IkinciClass(IMesajlaClass oIMesajlaClass)
+        {
+            _oIMesajlaClass = oIMesajlaClass;
+        }
+        public void MesajıGoster(string mesaj)
+        { 
+            if (_oIMesajlaClass == null)
+            {
+            Console.WriteLine("Ikinci class'tan mesaj : " + mesaj);
+            }
+            _oIMesajlaClass.MesajAt(mesaj);
+        }
+}
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿
+using System;
+
+
+
+
+namespace ConstructorInjectionProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+    {
+            IlkClass oIlkClass = new IlkClass();
+            IkinciClass oIkinciClass = new IkinciClass(oIlkClass);
+            oIkinciClass.MesajıGoster("Örbek bir mesaj");
+        Console.ReadLine();
+    }
+}}
